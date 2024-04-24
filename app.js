028f68e5e29f8ffb38d330ff3847d6aab98f4f9e
m.hamza hashim <m.hamzahashim1@gmail.com>
2024-04-25 01:31:47 +0500

    date completed

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-
-const now = moment();
-console.log(now.format()); // Output: 2024-04-25T00:00:00-07:00 (assuming your time zone is PST)
-document.write(moment().format());
-
-
-{/* <script src"moment.js"></script> */}
+let date = document.getElementById("date");
+date.innerHTML = moment().format('llll');
