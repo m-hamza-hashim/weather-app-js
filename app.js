15da8a090522e78db54fb668a68f327abc107eaf
m.hamza hashim <m.hamzahashim1@gmail.com>
2024-04-24 23:11:43 +0500

    nav completed

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const now = moment();
+console.log(now.format()); // Output: 2024-04-25T00:00:00-07:00 (assuming your time zone is PST)
+document.write(moment().format());
+
+
+{/* <script src"moment.js"></script> */}
