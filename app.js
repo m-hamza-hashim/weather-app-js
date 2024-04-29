cc720d17f657091b07469255a5a6741ffa2b9e80
m.hamza hashim <m.hamzahashim1@gmail.com>
2024-04-29 23:42:34 +0500

    completed

--- app.js
+++ app.js
@@ -1,2 +1,88 @@
+// date
 let date = document.getElementById("date");
-date.innerHTML = moment().format('llll');
+date.innerHTML = moment().format("llll");
+
+// temp
+let temp = document.getElementById("temp"), icon = document.getElementById("icon");
+
+// others 
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let num3 = document.getElementById("num3");
+
+// search
+let place = "Karachi,PK", search = document.getElementsByClassName("form-control")[0], searchBtn = document.getElementById("search-btn"), des = document.getElementById("des");
+
+const getPrintData = () => {
+  const getData = () => {
+    return new Promise((resolve, reject) => {
+      axios(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${
+          search.value || place
+        }?key=9E8RGKEWMD8YSCNMTLQ38NZ25&unitGroup=metric`
+      )
+        .then((res) => resolve(res.data))
+        .catch((err) => reject(err));
+    });
+  };
+
+  const printData = async () => {
+    try {
+      let res = await getData();
+      console.log(res);
+      let { days } = res;
+      let [day] = days;
+      temp.innerHTML = day.temp + "°";
+      des.innerHTML = day.conditions;
+      icon.src = `imgs/weather/${day.icon}.svg`;
+      num1.innerHTML = `${day.precipprob}%`;
+      num2.innerHTML = `${day.humidity}%`;
+      num3.innerHTML = `${day.windspeed}km/hr`;
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  printData();
+};
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Enter") {
+    getPrintData();
+  }
+});
+
+getPrintData();
+
+searchBtn.addEventListener("click", () => {
+  getPrintData();
+});
+
+// push notifications 
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", function () {
+//     navigator.serviceWorker
+//       .register("/serviceWorker.js")
+//       .then((res) => {
+//         console.log("service worker registered");
+//         Notification.requestPermission().then((res) => {
+//           if (Notification.permission == "granted") {
+//             console.log("Granted permission");
+//             return;
+//           }
+//           console.log(res);
+//         });
+//       })
+//       .catch((err) => console.log("service worker not registered", err));
+//   });
+//   navigator.serviceWorker.ready.then((swReg) => {
+//     var options = {
+//       body: "Hello",
+//       icon: "imgs/favicon.jpg",
+//     };
+//     swReg.showNotification("Greetings", options);
+//   });
+// }
+
+
---
+++ serviceWorker.js
@@ -0,0 +1,51 @@
+const staticDevCoffee = "dev-blog-app-js"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/imgs/icons/icon72.png",
+  "/imgs/icons/icon96.png",
+  "/imgs/icons/icon144.png",
+  "/imgs/icons/icon192.png",
+  "/imgs/icons/icon512.png",
+  "/imgs/favicon.jpg",
+  "imgs/weather/clear-day.svg",
+  "imgs/weather/clear-night.svg",
+  "imgs/weather/cloudy.svg",
+  "imgs/weather/fog.svg",
+  "imgs/weather/hail.svg",
+  "imgs/weather/partly-cloudy-day.svg",
+  "imgs/weather/partly-cloudy-night.svg",
+  "imgs/weather/rain.svg",
+  "imgs/weather/rain-snow.svg",
+  "imgs/weather/rain-snow-showers-day.svg",
+  "imgs/weather/rain-snow-showers-night.svg",
+  "imgs/weather/showers-day.svg",
+  "imgs/weather/showers-night.svg",
+  "imgs/weather/sleet.svg",
+  "imgs/weather/snow.svg",
+  "imgs/weather/snow-showers-day.svg",
+  "imgs/weather/snow-showers-night.svg",
+  "imgs/weather/thunder.svg",
+  "imgs/weather/thunder-rain.svg",
+  "imgs/weather/thunder-showers-day.svg",
+  "imgs/weather/thunder-showers-night.svg",
+  "imgs/weather/wind.svg",
+]
+
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+});
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+});
